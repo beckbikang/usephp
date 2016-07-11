e9ca44a8f9a1298ee5f5dcb5d7078f24e6844ef7
beck <cabing2005@gmail.com>
2016-07-11 14:18:17 +0800

    go

---
+++ learnphp/grammer/Grammer.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * 测试总结的文稿
+ */
+
+abstract class Person{
+    abstract   function say();
+}
+
+//测试构造函数和析构函数的调用
+class ParentClass extends Person{
+    
+    protected $name = "";   
+    public function __construct() {
+        echo "fathor  construct\n";
+    }
+    
+    public function who(){
+        return new static;
+    }
+    
+    public function who2(){
+        return new self;
+    }
+
+    public function foo(){
+        static::who();
+    }
+    
+    public function __destruct() {
+        echo "fathor  destruct\n";
+    }
+    public function say(){
+        echo "fathor\n";
+    }
+    
+}
+
+class Son extends ParentClass{
+    
+    /*
+    public function __construct() {
+        echo "son construct\n";
+    }*/
+    
+    public function __destruct() {
+        echo "son  destruct\n";
+    }
+    public function say(){
+        echo "son\n";
+    }
+}
+
+class Sister extends ParentClass{
+    public function say(){
+        echo "Sister\n";
+    }
+}
+
+function sayIt(ParentClass $obj){
+    $obj->say();
+}
+
+
+$obj  = new Son();
+//后期静态绑定
+$obj2 = $obj->who();
+$obj3 = $obj->who2();
+var_dump($obj2);
+var_dump($obj3);
+//多态
+$i_son = new Son();
+$i_sister = new Sister();
+sayIt($i_sister);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
